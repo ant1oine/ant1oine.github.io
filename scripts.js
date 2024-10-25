8412e8494cab69e2812aaac95eb85d77fc519ce1
Antoine <73118200+ant1oine@users.noreply.github.com>
2024-10-25 13:27:18 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-function validateLogin() {
-    const password = document.getElementById('password').value;
-    const unauthorizedMessage = document.getElementById('unauthorizedMessage');
-    const alarmSound = document.getElementById('alarmSound');
-
-    // Example validation
-    if (password === '123') {
-        alert('Login successful!');
-        // Redirect or perform other actions
-        return false; // Prevent form submission
-    } else {
-        unauthorizedMessage.style.display = 'block';
-        alarmSound.play();
-        setTimeout(() => {
-            unauthorizedMessage.style.display = 'none';
-            alarmSound.pause();
-            alarmSound.currentTime = 0;
-        }, 3000); // Show the message and play the sound for 3 seconds
-        return false; // Prevent form submission
-    }
-}
-
 function createRain() {
     const rainContainer = document.getElementById('rainContainer');
     const numberOfDrops = 100;
@@ -30,7 +8,7 @@ function createRain() {
         rainDrop.style.left = `${Math.random() * 100}vw`;
         rainDrop.style.animationDuration = `${Math.random() * 5 + 1}s`; // Slowed down the rain
         rainDrop.style.setProperty('--i', i);
-        rainDrop.innerText = '·';
+        rainDrop.innerText = 'hello';
         rainContainer.appendChild(rainDrop);
     }
 }
